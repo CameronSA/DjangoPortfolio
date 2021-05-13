9736ed675758b0d5d86ebcd4172eb8a95284774c
cameron simpson-allsop <cammeronallsop95@hotmail.co.uk>
2021-05-13 21:46:44 +0100

    added images to blog

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,26 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
+    title = models.CharField(max_length=255)    
+    cover = models.ImageField(upload_to='images/',default='null')
+    body1 = models.TextField(default='',blank=True)
+    image1 = models.ImageField(upload_to='images/',default='null')
+    body2 = models.TextField(default='',blank=True)
+    image2 = models.ImageField(upload_to='images/',default='null')
+    body3 = models.TextField(default='',blank=True)
+    image3 = models.ImageField(upload_to='images/',default='null')
+    body4 = models.TextField(default='',blank=True)
+    image4 = models.ImageField(upload_to='images/',default='null')
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
